b1ac02500a7e692899c939f4acc307d0f19210e5
technikmax-hgs <max-roensch@gmx.net>
2023-12-18 14:43:24 +0100

    Aufgbe 2

--- AufgabenBlatt_3/AufgabenBlatt_3.cpp
+++ AufgabenBlatt_3/AufgabenBlatt_3.cpp
@@ -1,20 +1,52 @@
 // AufgabenBlatt_3.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
 //
-
 #include <iostream>
+#include <math.h>
+using namespace std;
+
+
+
+void aufgabe_1();
+void aufgabe_2();
+void aufgabe_3();
+void aufgabe_4();
+void aufgabe_5();
+void aufgabe_6();
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//aufgabe_1();
+	//aufgabe_2();
+	aufgabe_3();
+	//aufgabe_4();
+	//aufgabe_5();
+	//aufgabe_6();
 }
 
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
 
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
+
+void aufgabe_2()
+{
+	int zeil = 0;
+	
+	int n = 1;
+	int z = 0;
+
+	for (zeil; zeil < 10; zeil++)
+	{
+		int spalte = 0;
+
+		for (spalte; spalte < 10; spalte++)
+		{
+			
+			cout << n << " ";
+			n++;
+		}
+		z++;
+		cout << endl; 
+		
+	}
+
+}
+
