0c0ad43d8038dab2412ebe9087772c07c73edef2
technikmax-hgs <max-roensch@gmx.net>
2023-12-18 17:10:10 +0100

    Zustandsmaschine Grundgekrüst

--- AufgabenBlatt_3/AufgabenBlatt_3.cpp
+++ AufgabenBlatt_3/AufgabenBlatt_3.cpp
@@ -50,3 +50,102 @@ void aufgabe_2()
 
 }
 
+void aufgabe_3()
+{
+	//zustand automat
+	enum Zeichen 
+	{
+		Start,
+		H,
+		A,
+		Fehler,
+		Ausgabe,
+		Uebergang,
+	};
+
+	char eingabe; 
+	int a = 0, h = 0; 
+	bool Z;
+	
+
+
+	Zeichen Zustand = Start; 
+
+	while (Zustand != Fehler && Zustand != Uebergang)
+	{
+			cout << "geben sie ein zeichen ein";
+			cin >> eingabe;
+
+			switch (Zustand)
+			{
+
+			case Start:
+				if (eingabe == 'h')
+				{
+					Zustand = H;
+					h++;
+				}
+				else
+				{
+					Zustand = Fehler;
+		
+				}
+				break;
+
+			case H:
+				if (eingabe == 'a')
+				{
+					Zustand = A;
+					a++;
+				}
+				else
+				{
+					Zustand = Fehler;
+				}
+				break;
+
+			case A:
+				if (eingabe == 'H')
+				{
+					Zustand = H;
+					Z = 1;
+				}
+				else if (eingabe == '!')
+				{
+					Zustand = Uebergang;
+					h++;
+				}
+				else
+				{
+					Zustand = Fehler;
+					break;
+				}
+
+			default: cout << "Dumm gelaufen ";
+
+			}
+	}
+	switch (Zustand)
+	{
+	case Fehler: cout << "Das Wort Entspricht nicht unserer Sprache";
+		break;
+	case Uebergang: cout << "Das Wort entspricht unseren Wünschen" << endl;
+		break;
+	default:
+		cout << "Fehler";
+	}
+
+	if (a == h && Z && !Fehler)
+	{
+		int zwisch = (a+h)/2 ; 
+		int j = 0; 
+		for (j; j > zwisch; j++)
+		{
+			cout << "ha";
+		}
+		cout << "!";
+	}
+
+
+
+	
